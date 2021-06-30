51423f37e3a8ca05ae9387c287a732ebdd170c8a
gregbugaj <gregbugaj@yahoo.com>
2021-06-30 16:10:01 -0500

    run.py

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ import cv2
 import argparse
 import numpy as np
 
+import hashlib
+
 from form.segmenter import FormSegmeneter
 from boxes.box_processor import BoxProcessor
 from form.numpyencoder import NumpyEncoder
@@ -18,7 +20,6 @@ def ensure_exists(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)   
     return dir   
-
 
 def current_milli_time():
     return round(time.time() * 1000)   
@@ -75,9 +76,10 @@ class FormProcessor:
         file_path = os.path.join(debug_dir, "text_over_segmask.png")
         cv2.imwrite(file_path, canvas_img)
 
+        # All models need to be rebuild
         # fields = ['HCFA02', 'HCFA33_BILLING', 'HCFA05_ADDRESS', 'HCFA05_CITY', 'HCFA05_STATE', 'HCFA05_ZIP', 'HCFA05_PHONE']
         fields = ['HCFA33_BILLING']
-        # fields = ['HCFA05_PHONE']
+        fields = ['HCFA02']
         
         print(f'All fields : {fields}')
         meta = {
@@ -99,7 +101,6 @@ class FormProcessor:
                 boxes, fragments, lines, _ = boxer.extract_bounding_boxes(id, field, snippet)
                 icr_results = icr.icr_extract(id, field, snippet, boxes, fragments, lines)
             except Exception as ident:
-                raise ident
                 failed = True
                 print(f'Field failed : {field}')
                 print(ident)
@@ -120,74 +121,46 @@ class FormProcessor:
             'fields': field_results,
         }
 
-        print(result) 
-        file_path = os.path.join(debug_dir, "results.json")
-        with open(file_path, 'w') as f:
-            json.dump(result, f,  sort_keys=True,  separators=(',', ': '), ensure_ascii=False, indent=4, cls=NumpyEncoder)
-            
+        print(f'Eval time : {m1}')
         return result
 
 def parse_args():
     """Parse input arguments"""
     parser = argparse.ArgumentParser(description='Run evaluator')
-    parser.add_argument('--img', dest='img_src', help='Image to evaluate', default='data/clean.png', type=str)
-    parser.add_argument('--output', dest='dir_out', help='Output directory evaluate', default='./data/debug', type=str)
-    parser.add_argument('--debug', dest='debug', help='Debug results', default=False, type=bool)
+    parser.add_argument('--img', dest='img_src', help='Image to evaluate', default='data/form.png', type=str)
+    parser.add_argument('--output_dir', dest='output_dir', help='Item result directory', default='./data/output', type=str)
+    parser.add_argument('--work_dir', dest='debug', help='Debug directory results', default='./data/debug', type=str)
 
     return parser.parse_args() 
+
+def main(img_path, output_dir, work_dir, cuda):
+    print('Main')
+    print(f'cuda       = {cuda}')
+    print(f'img_path   = {img_path}')
+    print(f'output_dir = {output_dir}')
+    print(f'work_dir   = {work_dir}')
+
+    processor = FormProcessor(work_dir=work_dir, cuda=cuda)
+    results = processor.process(img_path)
+
+    print(results) 
+
+    file_path = os.path.join(output_dir, "results.json")
+    print(f'Saving results to : {file_path}')
+    with open(file_path, 'w') as f:
+        json.dump(results, f,  sort_keys=True,  separators=(',', ': '), ensure_ascii=False, indent=4, cls=NumpyEncoder)
+
 
 if __name__ == '__main__':
     args = parse_args()
 
-    work_dir='/tmp/form-segmentation'
-    img_path='/home/greg/tmp/hicfa/PID_10_5_0_3101.original.tif'
+    args.img_src = '/home/greg/tmp/hicfa/PID_10_5_0_3101.original.tif'
+    args.work_dir = '/tmp/form-segmentation'
 
-    if True:
-        processor = FormProcessor(work_dir='/tmp/form-segmentation', cuda=False)
-        processor.process(img_path)
+    img_path = args.img_src
+    work_dir = args.work_dir
 
-    if False:
-        img_path='/home/greg/tmp/snippets/009.png'
-        
+    id = img_path.split('/')[-1]
+    output_dir = ensure_exists(os.path.join(work_dir, id, 'result'))
 
-        id = img_path.split('/')[-1]
-        debug_dir = ensure_exists(os.path.join(work_dir, id, 'work'))
-
-        snippet = cv2.imread(img_path)
-
-        fp = FieldProcessor(work_dir)
-        icr = IcrProcessor(work_dir)
-
-        # snippet_clean = fp.process(id, 'HCFA33_BILLING', snippet)
-        # snippet_clean = fp.process(id, 'HCFA05_PHONE', snippet)
-
-        boxer = BoxProcessor(work_dir, cuda=False)
-        boxes, img_fragments, lines, _= boxer.extract_bounding_boxes(id, 'field', snippet)
-        icr.icr_extract(id, 'HCFA05_PHONE', snippet, boxes, img_fragments, lines)
-
-    if False:
-        work_dir='/tmp/form-segmentation'
-        boxer = BoxProcessor(work_dir, cuda=False)
-
-        import glob
-        for name in glob.glob('/home/greg/tmp/single/*.jpg'):
-            print(name)
-            id = name.split('/')[-1]
-            debug_dir = ensure_exists(os.path.join(work_dir, id, 'work'))
-            snip = read_image(name)
-            if snip is None:
-                continue
-            boxes, fragments, lines, _= boxer.extract_bounding_boxes(id, 'field', snip)
-            icr(id, snip, boxes, fragments)
-
-    if False:
-        import glob
-        # for name in glob.glob('/tmp/hicfa/*.tif'):
-        # for name in glob.glob('/home/greg/tmp/task_3100-3199-2021_05_26_23_59_41-cvat/images/*.tif'):
-        for name in glob.glob('/home/greg/tmp/task_3100-3199-2021_05_26_23_59_41-cvat/images/PID_10_5_0_3129.original.tif'):
-            try:
-                print(name)
-                segment(name)
-                break
-            except Exception as ident:
-                print(ident)
+    main(img_path=img_path, output_dir=output_dir, work_dir=work_dir, cuda=False)
